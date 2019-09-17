71949434f2d9433d58defa049412cafadc529790
andrewc <50561935+andrewcopenpilot@users.noreply.github.com>
2019-09-16 22:09:32 -0700

    Full hardware tested good

--- board/drivers/can.h
+++ board/drivers/can.h
@@ -17,13 +17,13 @@ powertrain: 0x1F1: body control module
 powertrain: 0x140: body control module
 
 
-chassis: 0xC1: electronic brake control module
-chassis: 0xC5: electronic brake control module
+chassis: 0xc1: electronic brake control module
+chassis: 0xc5: electronic brake control module
 chassis: 0x130: multi axis accelerometer
 chassis: 0x140: multi axis accelerometer
 chassis: 0x170: electronic brake control module
-chassis: 0x1E5: steering angle sensor
-chassis: 0xC0: Seems to be needed for auto highbeams
+chassis: 0x1e5: steering angle sensor
+chassis: 0xc0: seems to be needed for auto highbeams
 */
 
 // IRQs: CAN1_TX, CAN1_RX0, CAN1_SCE, CAN2_TX, CAN2_RX0, CAN2_SCE, CAN3_TX, CAN3_RX0, CAN3_SCE
@@ -140,28 +140,21 @@ void can_init(uint8_t can_number) {
   // no mask
   CAN->FS1R|=CAN_FS1R_FSC0;
 
-  if (can_number == 99) {
-    CAN->sFilterRegister[0].FR1 = 0x24B<<21;
-    CAN->sFilterRegister[0].FR2 = 0xF1<<21;
-    CAN->sFilterRegister[1].FR1 = 0xC9<<21;
-    CAN->sFilterRegister[1].FR2 = 0x1E9<<21;
-    CAN->sFilterRegister[2].FR1 = 0x1C4<<21;
-    CAN->sFilterRegister[2].FR2 = 0x1C5<<21;
-    CAN->sFilterRegister[3].FR1 = 0x1F5<<21;
-    CAN->sFilterRegister[3].FR2 = 0x1E1<<21;
-    CAN->sFilterRegister[4].FR1 = 0x214<<21;
-    CAN->sFilterRegister[4].FR2 = 0x230<<21;
-    CAN->sFilterRegister[5].FR1 = 0x34A<<21;
-    CAN->sFilterRegister[5].FR2 = 0x12A<<21;
-    CAN->sFilterRegister[6].FR1 = 0x135<<21;
-    CAN->sFilterRegister[6].FR2 = 0x184<<21;
-    CAN->sFilterRegister[7].FR1 = 0x1F1<<21;
-    CAN->sFilterRegister[7].FR2 = 0x140<<21;
+  if (can_number == 0) {
+    CAN->sFilterRegister[0].FR1 = 0xC1<<21;
+    CAN->sFilterRegister[0].FR2 = 0xC5<<21;
+    CAN->sFilterRegister[1].FR1 = 0x130<<21;
+    CAN->sFilterRegister[1].FR2 = 0x140<<21;
+    CAN->sFilterRegister[2].FR1 = 0x170<<21;
+    CAN->sFilterRegister[2].FR2 = 0x1E5<<21;
+    CAN->sFilterRegister[3].FR1 = 0xC0<<21;
+    CAN->sFilterRegister[3].FR2 = 0xC0<21;
     CAN->sFilterRegister[14].FR1 = 0;
     CAN->sFilterRegister[14].FR2 = 0;
-    CAN->FM1R |= CAN_FM1R_FBM0 | CAN_FM1R_FBM1 | CAN_FM1R_FBM2 | CAN_FM1R_FBM3 | CAN_FM1R_FBM4 | CAN_FM1R_FBM5 | CAN_FM1R_FBM6 | CAN_FM1R_FBM7;
-    CAN->FA1R |= CAN_FA1R_FACT0 | CAN_FA1R_FACT1 | CAN_FA1R_FACT2 | CAN_FA1R_FACT3 | CAN_FA1R_FACT4 | CAN_FA1R_FACT5 | CAN_FA1R_FACT6 | CAN_FA1R_FACT7;
-    CAN->FA1R |= 1 | (1 << 14);
+    CAN->FM1R |= CAN_FM1R_FBM0 | CAN_FM1R_FBM1 | CAN_FM1R_FBM2 | CAN_FM1R_FBM3;
+    CAN->FA1R |= CAN_FA1R_FACT0 | CAN_FA1R_FACT1 | CAN_FA1R_FACT2 | CAN_FA1R_FACT3;
+    CAN->FA1R |= (1 << 14);
+    CAN->FFA1R = 0x00000000;
   }
   else {
     CAN->sFilterRegister[0].FR1 = 0;
@@ -252,17 +245,6 @@ void process_can(uint8_t can_number) {
     if ((CAN->TSR & CAN_TSR_TME0) == CAN_TSR_TME0) {
       // add successfully transmitted message to my fifo
       if ((CAN->TSR & CAN_TSR_RQCP0) == CAN_TSR_RQCP0) {
-        //can_txd_cnt += 1;
-
-        //if ((CAN->TSR & CAN_TSR_TXOK0) == CAN_TSR_TXOK0) {
-        //  CAN_FIFOMailBox_TypeDef to_push;
-        //  to_push.RIR = CAN->sTxMailBox[0].TIR;
-        //  to_push.RDTR = (CAN->sTxMailBox[0].TDTR & 0xFFFF000F) | ((CAN_BUS_RET_FLAG | bus_number) << 4);
-        //  to_push.RDLR = CAN->sTxMailBox[0].TDLR;
-        //  to_push.RDHR = CAN->sTxMailBox[0].TDHR;
-        //  can_push(&can_rx_q, &to_push);
-        //}
-
         if ((CAN->TSR & CAN_TSR_TERR0) == CAN_TSR_TERR0) {
           #ifdef DEBUG
             puts("CAN TX ERROR!\n");
@@ -281,7 +263,6 @@ void process_can(uint8_t can_number) {
       }
 
       if (can_pop(can_queues[bus_number], &to_send)) {
-        //can_tx_cnt += 1;
         // only send if we have received a packet
         CAN->sTxMailBox[0].TDLR = to_send.RDLR;
         CAN->sTxMailBox[0].TDHR = to_send.RDHR;
@@ -384,93 +365,15 @@ int fwd_filter(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
   uint32_t addr = to_fwd->RIR>>21;
 
   if (bus_num == 0) {
-
-
-// ######### PT ###################
-    if ((addr == 0x24B) ||
-        (addr == 0xF1) ||
-	(addr == 0xC9) ||
-	(addr == 0x1E9) ||
-	(addr == 0x1C4) ||
-	(addr == 0x1C5) ||
-	(addr == 0x1F5) ||
-	(addr == 0x1E1) ||
-	(addr == 0x214) ||
-	(addr == 0x230) ||
-	(addr == 0x34A) ||
-	(addr == 0x12A) ||
-	(addr == 0x135) ||
-	(addr == 0x184) ||
-	(addr == 0x1F1) ||
-	(addr == 0x140)) {
-      return 2;
-    }
-    return -1;
-
-    //puts("\n");
-    //puth(addr);
-
-//    if ((addr == 0xC1) || (addr == 0xC5) || (addr == 0xD1) || (addr == 0x1C8) || (addr == 0x1E5) || (addr == 0xBE)
-//       || (addr == 0xC7) || (addr == 0xD3) || (addr == 0xF9) || (addr == 0x18E)|| (addr == 0x191) || (addr == 0x1E7)
-//       || (addr == 0x1EB) || (addr == 0x1ED) || (addr == 0x1C7) || (addr == 0x1CE) || (addr == 0x189)
-//       || (addr == 0x197) || (addr == 0x19D) || (addr == 0x1A1) || (addr == 0x1A3) || (addr == 0x1A6) || (addr == 0x1AA)
-//       || (addr == 0x1AF) || (addr == 0x1BA) || (addr == 0x1C3) || (addr == 0xC4) || (addr == 0x1DF) || (addr == 0x287)
-//       || (addr == 0x321) || (addr == 0x1F3) || (addr == 0x2CD) || (addr == 0x2CF) || (addr == 0x365)
-//       || (addr == 0x372) || (addr == 0x1F4) || (addr == 0x1FC) || (addr == 0x1FE) || (addr == 0x210) || (addr == 0x232)
-//       || (addr == 0x2C3) || (addr == 0x2D3) || (addr == 0x29F) || (addr == 0x348) || (addr == 0x34C) || (addr == 0x362)
-//       || (addr == 0x137) || (addr == 0x139) || (addr == 0x142)  || (addr == 0x160)  || (addr == 0x170)
-//       || (addr == 0x17D) || (addr == 0x182) || (addr == 0x216) 
-//       || (addr == 0x22A) || (addr == 0x234) || (addr == 0x260) || (addr == 0x261) || (addr == 0x262) || (addr == 0x263) || (addr == 0x264)
-//       || (addr == 0x265) || (addr == 0x2F1) || (addr == 0x32A) || (addr == 0x37A) || (addr == 0x37C) || (addr == 0x37D) || (addr == 0x3C1) || (addr == 0x3C7)
-//       || (addr == 0x3C7) || (addr == 0x3C9) || (addr == 0x3D1) || (addr == 0x3D3) || (addr == 0x3E9) || (addr == 0x409)
-//       || (addr == 0x40A) || (addr == 0x451) || (addr == 0x510) || (addr == 0x120) || (addr == 0x130)
-//       || (addr == 0x148) || (addr == 0x233) || (addr == 0x235) || (addr == 0x237) || (addr == 0x23D) || (addr == 0x241)
-//       || (addr == 0x324) || (addr == 0x37E) || (addr == 0x3ED) || (addr == 0x3F1) || (addr == 0x3F3) || (addr == 0x3F5)
-//       || (addr == 0x3F9) || (addr == 0x3FB) || (addr == 0x3FC) || (addr == 0x4C1) || (addr == 0x4C5) || (addr == 0x4C7)
-//       || (addr == 0x4C9) || (addr == 0x4D1) || (addr == 0x4D9) || (addr == 0x4E1) || (addr == 0x4E9) || (addr == 0x4EB)
-//       || (addr == 0x4ED) || (addr == 0x4EF) || (addr == 0x4F1) || (addr == 0x4F3) || (addr == 0x4F7) || (addr == 0x500)
-//       || (addr == 0x514) || (addr == 0x52A) || (addr == 0x52B) || (addr == 0x530) || (addr == 0x589) || (addr == 0x641)
-//       || (addr == 0x652) || (addr == 0x770) || (addr == 0x772) || (addr == 0x773) || (addr == 0x778) || (addr == 0x77C)
-//       || (addr == 0x77D) || (addr == 0x77E) || (addr == 0x77F) || (addr == 0x780) || (addr == 0x78A) || (addr == 0x7E0) || (addr == 0x7E8)
-//       || (addr == 0x2F9) 
-//      ) {
-//      return -1;
-//    }
-//    puts("\n");
-//    puth(addr);
-//    return 2; 
-
-
-// ########### END PT ###############
-
-
-// ######### Chassis ####################
-//    if ((addr == 0xC0) || (addr == 0xC1) || (addr == 0xC5) || (addr == 0x130) || (addr == 0x140) || (addr == 0x170) || (addr == 0x1E5)) {
-//      return 2;
-//    }
-//    return -1;
-    //if ((addr == 0xC9) || (addr == 0x160) || (addr == 0x323) || (addr == 0x180) || (addr == 0x1CE) || (addr == 0x1D5) || (addr == 0x1E9)
-    //     || (addr == 0x212) || (addr == 0x234) || (addr == 0x335) || (addr == 0x320) || (addr == 0x321) || (addr == 0x325)
-//	 || (addr == 0x1FC) || (addr == 0x348) || (addr == 0x34A) || (addr == 0x362) || (addr == 0x365) || (addr == 0x121)
-//	 || (addr == 0x122) || (addr == 0x12A) || (addr == 0x12C) || (addr == 0x17D) || (addr == 0x182) || (addr == 0x1CA)
-//	 || (addr == 0x1F5) || (addr == 0x200) || (addr == 0x238) || (addr == 0x340) || (addr == 0x3E9) || (addr == 0x3EB) || (addr == 0x148)
-//	 || (addr == 0x4E1) || (addr == 0x514)          || (addr == 0xC0)) {
-//      return -1; 
-//    }
-//    puts("\n");
-//    puth(addr);
-//    return 2;
-
-// ########## end chassis ###############
-
+    return 2;
   }
 
   if (bus_num == 2) {
     // Drop all steering + brake messages
-    //if ((addr == 384) || (addr == 715)  || (addr == 789) || (addr == 880)) {
+    if ((addr == 384) || (addr == 715)  || (addr == 789) || (addr == 880)) {
 
     // Drop all steering messages
-    if ((addr == 384)) {
+    //if ((addr == 384)) {
         return -1;
     }
 
@@ -479,38 +382,6 @@ int fwd_filter(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
 
   return -1;
 }
-
-//void can_tx(uint8_t can_number, CAN_FIFOMailBox_TypeDef *to_tx) {
-//  CAN_TypeDef *CAN = CANIF_FROM_CAN_NUM(can_number);
-//  uint8_t transmitmailbox;
-//
-//  /* Check that all the Tx mailioxes are not full */
-//  if (((CAN->TSR & CAN_TSR_TME0) != RESET) ||
-//      ((CAN->TSR & CAN_TSR_TME1) != RESET) ||
-//      ((CAN->TSR & CAN_TSR_TME2) != RESET))
-//  {
-//    transmitmailbox = (CAN->TSR & CAN_TSR_CODE) >> CAN_TSR_CODE_Pos;
-//
-//    CAN->sTxMailBox[transmitmailbox].TDLR = to_tx->RDLR;
-//    CAN->sTxMailBox[transmitmailbox].TDHR = to_tx->RDHR;
-//    CAN->sTxMailBox[transmitmailbox].TDTR = to_tx->RDTR;
-//    CAN->sTxMailBox[transmitmailbox].TIR = to_tx->RIR;
-//
-//    if (can_number == 0) {
-//      can0_tx_cnt++;
-//    }
-//    if (can_number == 2) {
-//      can2_tx_cnt++;
-//    }
-//    return;
-//  }
-//  if (can_number == 0) {
-//    can0_mailbox_full_cnt++;
-//  }
-//  if (can_number == 2) {
-//    can0_mailbox_full_cnt++;
-//  }
-//}
 
 // CAN receive handlers
 // blink blue when we are receiving CAN messages
@@ -540,8 +411,8 @@ void can_rx(uint8_t can_number) {
     int bus_fwd_num = fwd_filter(bus_number, &to_tx);
     if (bus_fwd_num != -1) {
       //can_tx(bus_fwd_num, &to_tx);
-      can_push(can_queues[bus_number], &to_tx);
-      process_can(CAN_NUM_FROM_BUS_NUM(bus_number));
+      can_push(can_queues[bus_fwd_num], &to_tx);
+      process_can(CAN_NUM_FROM_BUS_NUM(bus_fwd_num));
     }
 
     // next
